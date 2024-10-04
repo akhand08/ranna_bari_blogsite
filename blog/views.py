8731acf53cecb1338fac7a0007791ecca169e5a6
akhand08 <adib08@outlook.com>
2024-10-04 15:33:13 +0600

    Comment-On-The-Post feature has been added along with completion of the 2nd chapter

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+
+from django.views.decorators.http import require_POST
 
 from django.views.generic import ListView
 # Create your views here.
@@ -32,7 +34,10 @@ def post_detail(request, year, month, day, post):
                              publish__month = month,
                              publish__day = day)
     
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments =  post.comments.filter(active=True)
+    form = CommentForm()
+    
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
     
 
 class PostListView(ListView):
@@ -64,3 +69,18 @@ def post_share(request, post_id):
         form = EmailPostForm()
     
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status = Post.Status.PUBLISHED )
+    comment = None
+    
+    form = CommentForm(data = request.POST)
+    
+    if form.is_valid():
+        comment = form.save(commit = False)
+        comment.post = post
+        
+        comment.save()
+        
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
